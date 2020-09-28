193b7f72891a33495dfced9809e006be10fb44c8
habelash <habelash@gmail.com>
2020-09-29 00:41:14 +0530

    marking attendence in .csv file

--- detection.py
+++ detection.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 path = 'TrainImages'
 images = []
@@ -25,8 +26,22 @@ def findEncodings(images):
     return encodeList
 
 
+def markAttendence(name):
+    with open('Attendence.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name}, {dtString}')
+
+
 encodeListKnown = findEncodings(images)
-print('Encoding Complete')
+print(len(encodeListKnown), 'Encoding Complete')
+# print('Encoding Complete')
 
 cap = cv2.VideoCapture(0)
 
@@ -46,12 +61,13 @@ while True:
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
-            print(name)
+            # print(name)
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            markAttendence(name)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
